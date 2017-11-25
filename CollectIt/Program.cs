66cd9059a531e1f231e86699a21ac3de8b4fa04f
maciej.wolejszo <maciej.wolejszo@gmail.com>
2017-11-25 12:40:29 +0100

    Initial Commit;

--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -10,6 +10,25 @@ namespace CollectIt
     {
         static void Main(string[] args)
         {
+            Employee[] employees = new Employee[]
+            {
+                new Employee { Name = "Scott"},
+                new Employee { Name = "Alex"},
+                new Employee { Name = "Maciej"},
+                new Employee { Name = "Wojciech"},
+                new Employee { Name = "Natalia"},
+                new Employee { Name = "Marysia"}
+            };
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+            for (int i = 0; i< employee.Lenght; i++)
+            {
+                Console.WriteLine(employees[i].Name);
+            }
+            
         }
     }
 }
